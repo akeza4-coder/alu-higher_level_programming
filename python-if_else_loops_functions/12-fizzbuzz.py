a421d3c5f3be9d8326ca9208e92ef1d5316db613
akeza4-coder <your_email@example.com>
2026-02-26 04:07:41 -0800

    Add Task 12: FizzBuzz function

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     last_number = abs(number) % 10
-    print(last_number, end="")
+    print("{}".format(last_number), end="")
     return last_number
