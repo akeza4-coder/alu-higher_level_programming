6cb61054fb42a7d1ed8aeee9d60e69b4fc172183
akeza4-coder <your_email@example.com>
2026-03-04 01:20:19 -0800

    Replace element in a copy

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """Replaces an element in a list without modifying the original."""
+    # Create a copy of the list first
+    copy = my_list[:]
+
+    if idx < 0 or idx >= len(my_list):
+        return copy
+
+    copy[idx] = element
+    return copy
