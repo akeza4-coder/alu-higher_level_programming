2cce87154617b69f66dea4169d01281ff104b73b
akeza4-coder <your_email@example.com>
2026-03-21 14:24:10 -0700

    Add Task 6: raise_exception_msg and final project cleanup

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module for raising a name exception with a message.
+"""
+
+
+def raise_exception_msg(message=""):
+    """
+    Raises a NameError with a specific message.
+    """
+    raise NameError(message)
