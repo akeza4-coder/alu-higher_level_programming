3eca2dbea28c7d98743523c2c768d6edd9e4e669
akeza4-coder <your_email@example.com>
2026-04-02 13:19:45 -0700

    Add area and perimeter methods

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -45,7 +45,9 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        """Calculates and returns the rectangle perimeter."""
+        """Calculates and returns the rectangle perimeter.
+        Returns 0 if width or height is 0.
+        """
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width * 2) + (self.__height * 2)
