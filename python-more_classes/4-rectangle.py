b782c1974233542331aa553c6e0589816f416b3a
akeza4-coder <your_email@example.com>
2026-04-02 12:45:56 -0700

    Add area and perimeter methods to Rectangle

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle class with str and repr support.
+"""
+
+
+class Rectangle:
+    """Defines a rectangle by width and height."""
+
+    def __init__(self, width=0, height=0):
+        """Initializes the rectangle."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieves the width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width with validation."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieves the height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height with validation."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns the rectangle area."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Returns the rectangle perimeter."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        """Returns a string of the rectangle using #."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return (("#" * self.__width + "\n") * self.__height)[:-1]
+
+    def __repr__(self):
+        """Returns a string representation to recreate the instance."""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
--- python-more_classes/README.md
+++ python-more_classes/README.md
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 This project covers Object-Oriented Programming with Python, specifically focusing on classes and instances.
+=======
+This project expands on Python classes by adding methods for Area and Perimeter.
+>>>>>>> Add area and perimeter methods to Rectangle
