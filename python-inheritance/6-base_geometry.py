b7b53127fb5fd4d403674a53f93db02d63690378
akeza4-coder <your_email@example.com>
2026-04-03 09:46:28 -0700

    Update BaseGeometry with area method raising Exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module for BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class with an area method
+    """
+
+    def area(self):
+        """
+        Public instance method that raises an Exception.
+        """
+        raise Exception("area() is not implemented")
