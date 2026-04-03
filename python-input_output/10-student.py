3b06ce0a9466008cd277bc4418506e3e12a33405
akeza4-coder <your_email@example.com>
2026-04-03 10:21:37 -0700

    Add Task 10: Student class with filtered to_json

---
+++ python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module for Student class with filter.
+"""
+
+
+class Student:
+    """
+    Defines a student by first_name, last_name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the student instance.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attributes name contained
+        in this list must be retrieved. Otherwise, all attributes.
+        """
+        if isinstance(attrs, list) and all(isinstance(s, str) for s in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
