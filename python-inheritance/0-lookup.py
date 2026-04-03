6ce8c2921b827df62f92eb8267edbbc76e099905
akeza4-coder <your_email@example.com>
2026-04-03 04:45:27 -0700

    Add README and fix formatting for 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -8,11 +8,5 @@ available attributes and methods of an object.
 def lookup(obj):
     """
     Returns a list of available attributes and methods of an object.
-
-    Args:
-        obj: The object to inspect.
-
-    Returns:
-        A list of strings representing attributes and methods.
     """
     return dir(obj)
