7be3947b4dc5b01bc53fa1824f192e1a1150e6c8
akeza4-coder <your_email@example.com>
2026-04-03 04:55:18 -0700

    Fix documentation and PEP8 for MyList

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,35 +1,16 @@
 #!/usr/bin/python3
 """
-Module for MyList class
+This module contains a class MyList that inherits from list.
 """
 
 
 class MyList(list):
     """
-    MyList class that inherits from list
+    MyList class that inherits from list and adds a sorting method.
     """
 
     def print_sorted(self):
         """
-        Prints the list, but sorted (ascending sort)
-        """
-        print(sorted(self))
-EOFcd /alu-higher_level_programming/python-inheritance
-
-cat <<EOF > 1-my_list.py
-#!/usr/bin/python3
-"""
-Module for MyList class
-"""
-
-
-class MyList(list):
-    """
-    MyList class that inherits from list
-    """
-
-    def print_sorted(self):
-        """
-        Prints the list, but sorted (ascending sort)
+        Prints the list, but sorted in ascending order.
         """
         print(sorted(self))
