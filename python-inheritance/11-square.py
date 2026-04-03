632a8734d9a3e443051beff009632868bf5224b0
akeza4-coder <your_email@example.com>
2026-04-03 09:57:07 -0700

    Add Square class with custom __str__ representation

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module for Square class that inherits from Rectangle
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from Rectangle
+    """
+
+    def __init__(self, size):
+        """
+        Initializes the Square with size.
+
+        Args:
+            size (int): The side length of the square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square.
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        Returns the string representation of the Square.
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
