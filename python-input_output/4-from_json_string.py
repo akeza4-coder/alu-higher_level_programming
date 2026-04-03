94a19c3e74b51521d1695211515b9a9f455f187b
akeza4-coder <your_email@example.com>
2026-04-03 10:09:09 -0700

    Add Task 4: from_json_string function

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contains a function that returns an object represented
+by a JSON string.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns an object (Python data structure) represented by a JSON string.
+
+    Args:
+        my_str (str): The JSON string to be deserialized.
+
+    Returns:
+        The Python object representation of the JSON string.
+    """
+    return json.loads(my_str)
